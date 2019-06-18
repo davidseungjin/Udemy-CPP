815b8c2e326a5c6d540d0370bcac78ce75cf7f79
davidseungjin <ryonlee@gmail.com>
2019-06-18 11:58:27 -0700

    cpp

---
+++ Section17/smart_pointerII.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+class SmartPtr{
+	T *ptr;
+public:
+	explicit SmartPtr(T *p = NULL){
+		ptr = p;
+	}
+
+	~SmartPtr(){
+		delete(ptr);
+	}
+
+	T &operator *(){
+		return *ptr;
+	}
+
+	T *operator ->(){
+		return ptr;
+	}
+};
+
+int main(){
+	SmartPtr<int> ptr(new int());
+	*ptr = 20;
+	cout << *ptr;
+	return 0;
+}
+
